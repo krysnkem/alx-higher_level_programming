b7a5e8a6108fa02744a590469345109171665406
Christian Onyisi <krysnkem@gmail.com>
2022-12-27 22:04:41 +0000

    Correct a python script that slices words from a sentence to make a new sentence

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -2,5 +2,4 @@
 str = "Python is an interpreted, interactive, object-oriented programming\
        language that combines remarkable power with very clear syntax"
 str = f"{str[:-17]}"
-str = f"{str[39:66]} {str[113:117]} {str[:6]}"
-print(str)
+print(f"{str[39:66]} {str[113:117]} {str[:6]}")
