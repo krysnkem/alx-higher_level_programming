1f23f2eeb61dc61eb086a7785fe08889d399f27f
Christian Onyisi <krysnkem@gmail.com>
2023-01-09 15:11:18 +0000

    Correct a python script that prints the value of importing and runnning function add from from add_0 module

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-import add_0 as addMod
+from add_0 import add
 
 a = 1
 b = 2
-add = addMod.add
+
 if __name__ == "__main__":
     print("{} {} {} {} {}".format(a, "+", b, "=", add(a, b)))
