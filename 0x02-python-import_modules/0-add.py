f9edfc7e3215487d87c920435c6906a13fcf9ec3
Christian Onyisi <krysnkem@gmail.com>
2023-01-09 13:47:55 +0000

    Add a python script that imports and runs a function called add from add_0.py

---
+++ 0x02-python-import_modules/0-add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+import add_0 as addMod
+
+a = 1
+b = 2
+add = addMod.add
+if __name__ == "__main__":
+    print(a, "+", b, "=", add(a, b))
+
---
+++ 0x02-python-import_modules/README.md
@@ -0,0 +1,1 @@
+A directory for answers to 0x02-python-import_modules task of alx SE programme
