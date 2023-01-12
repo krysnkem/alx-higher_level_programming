3d19b2a286e7eed9651a64ba83bcc06617df5022
Christian Onyisi <onyisichrisitian@gmail.com>
2023-01-12 15:18:43 +0000

    Add Solution to 6. Lists of lists = Matrix

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+
+def print_matrix_integer(matrix=[[]]):
+    matrix_len = len(matrix)
+    last_row_idx = matrix_len - 1
+    for row_index in range(matrix_len):
+        row_length = len(matrix[row_index])
+        last_elem_idx = row_length - 1
+        for idx in range(row_length):
+            print("{:d}".format(matrix[row_index][idx]), end="")
+            if idx < last_elem_idx:
+                print("", end=" ")
+
+        if row_index < last_row_idx:
+            print()
+
+    print()
