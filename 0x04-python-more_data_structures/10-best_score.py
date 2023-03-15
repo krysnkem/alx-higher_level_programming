55219bd2a7171872aeaf3a6feda2846dc2c86d51
Christian Onyisi <krysnkem@gmail.com>
2023-03-15 14:53:13 +0000

    Change content of 10-best_score.py

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary is None or len(a_dictionary) == 0:
+
+    if not a_dictionary:
         return None
+
     return max(a_dictionary)
