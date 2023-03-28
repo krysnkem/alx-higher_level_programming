5c7ef6f35ee88ab160f17c02a49bd0b4c264b36e
Christian Onyisi <krysnkem@gmail.com>
2023-03-28 21:06:23 +0000

    Add solution for task 1: a class Square that defines a square by: (based on 0-square.py)

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+"""Solution to task1"""
+
+class Square:
+    """Defines a square with a size"""
+    def __init__(self, size):
+        """initializes square with private instance attribute called size"""
+        self.__size = size
