ac0f70a849c5e552f0ed7891889721124e1c43b7
Christian Onyisi <krysnkem@gmail.com>
2023-03-28 22:15:41 +0000

    Add solution for task 4:  Access and update private attribute

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+
+"""Solution to task 3"""
+
+
+class Square:
+
+    """Defines a square with a size"""
+    def __init__(self, size=0):
+        """initializes square with private instance attribute called size"""
+        if not size:
+            self.__size = 0
+        else:
+            self.size = size
+
+    @property
+    def size(self):
+        """gets size and returns it to the caller"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """checks and sets the value to size"""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def area(self):
+        """get the area of the square"""
+        return self.__size * self.__size
